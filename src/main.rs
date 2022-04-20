2880327c9b33f6dba2b277c733afb2e3e8e90672
moba1 <moba2@protonmail.com>
2022-04-21 00:52:10 +0900

    add loader

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+mod scriptlets;
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(parse(try_from_str=parse_path))]
+    config_directory: std::path::PathBuf,
+}
+
+fn parse_path(s: &str) -> Result<std::path::PathBuf, String> {
+    let mut path = std::path::PathBuf::new();
+    path.push(s);
+    Ok(path)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let config_directory = &args.config_directory;
+    match scriptlets::load(config_directory) {
+        Ok(_) => {},
+        Err(error) => println!("cannot load configs from {} [{}]", config_directory.display(), error),
+    };
 }
