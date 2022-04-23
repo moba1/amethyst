e119f5a76d25ab90badfe18594286270a29fb45e
moba1 <moba2@protonmail.com>
2022-04-24 01:01:07 +0900

    add non_exhaustive to enum

--- src/config/module.rs
+++ src/config/module.rs
@@ -2,6 +2,7 @@ use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 #[serde(untagged)]
+#[non_exhaustive]
 pub enum Module {
     File(String),
     Inline(super::scriptlet::ScriptLet),
--- src/config/scriptlet.rs
+++ src/config/scriptlet.rs
@@ -2,6 +2,7 @@ use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 #[serde(tag = "type")]
+#[non_exhaustive]
 pub enum ScriptLet {
     #[serde(rename(deserialize = "add"))]
     Add { source: String, destination: String },
