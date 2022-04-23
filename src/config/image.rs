2f626b9d607f194495b68a13aedd94d912f165a3
moba1 <moba2@protonmail.com>
2022-04-24 01:10:34 +0900

    add image name

--- src/config/image.rs
+++ src/config/image.rs
@@ -5,6 +5,7 @@ pub struct Image {
     modules: Vec<super::module::Module>,
     #[serde(default = "default_base_image")]
     base_image: String,
+    name: String,
 }
 
 fn default_base_image() -> String {
