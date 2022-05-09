ffb78a2f5dd004d5524b67fdad089c15a45468f9
moba1 <moba2@protonmail.com>
2022-05-09 17:24:53 +0900

    add image test

--- src/config/image.rs
+++ src/config/image.rs
@@ -5,6 +5,7 @@ use crate::result;
 use serde::{de, Deserialize, Deserializer, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
+#[cfg_attr(test, derive(PartialEq))]
 pub struct Image<Script> {
     pub scripts: Vec<Script>,
     #[serde(default)]
@@ -18,7 +19,7 @@ pub struct Image<Script> {
 impl Image<super::module::Module> {
     pub fn slurp_scriptlets(&self) -> result::Result<Vec<super::scriptlet::Scriptlet>> {
         let scriptlets = (&self.scripts)
-            .into_iter()
+            .iter()
             .map(|module| module.to_scriptlets())
             .collect::<Result<Vec<_>, _>>()?
             .concat();
@@ -42,4 +43,118 @@ where
 
 fn default_tag() -> String {
     tag::LATEST_TAG.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_deserialize() {
+        let scripts: Vec<i32> = vec![];
+        let original_string = format!(
+            r#"
+scripts: {}
+"#,
+            format_args!(
+                "[{}]",
+                scripts
+                    .iter()
+                    .map(|v| format!("{}", v))
+                    .collect::<Vec<_>>()
+                    .join(",")
+            ),
+        );
+        assert!(serde_yaml::from_str::<super::Image<i32>>(&original_string).is_err());
+
+        let scripts: Vec<i32> = vec![1];
+        let image_name = "amethyst";
+        let original_string = format!(
+            r#"
+scripts: {}
+name: {}
+"#,
+            format_args!(
+                "[{}]",
+                scripts
+                    .iter()
+                    .map(|v| format!("{}", v))
+                    .collect::<Vec<_>>()
+                    .join(",")
+            ),
+            image_name,
+        );
+        let deserialized_image = serde_yaml::from_str::<super::Image<i32>>(&original_string)
+            .expect("deserialized image struct");
+        let image = super::Image::<_> {
+            base_image: super::typ::ImageType::Scratch,
+            name: image_name.to_string(),
+            scripts,
+            tag: super::tag::LATEST_TAG.to_string(),
+        };
+        assert_eq!(image, deserialized_image);
+
+        let scripts: Vec<i32> = vec![1, 2];
+        let image_name = "amethyst";
+        let image_tag = "tag";
+        let base_image_name = "base_image";
+        let original_string = format!(
+            r#"
+scripts: {}
+name: {}
+tag: {}
+base_image:
+  name: {}
+"#,
+            format_args!(
+                "[{}]",
+                scripts
+                    .iter()
+                    .map(|v| format!("{}", v))
+                    .collect::<Vec<_>>()
+                    .join(",")
+            ),
+            image_name,
+            image_tag,
+            base_image_name,
+        );
+        let deserialized_image = serde_yaml::from_str::<super::Image<i32>>(&original_string)
+            .expect("deserialized image struct");
+        let image = super::Image::<_> {
+            base_image: super::typ::ImageType::BaseImage {
+                name: base_image_name.to_string(),
+                tag: super::tag::LATEST_TAG.to_string(),
+            },
+            name: image_name.to_string(),
+            scripts,
+            tag: image_tag.to_string(),
+        };
+        assert_eq!(image, deserialized_image);
+    }
+
+    #[test]
+    fn test_serialize() {
+        let image_name = "amethyst";
+        let image_tag = super::tag::LATEST_TAG;
+        let original_image = super::Image::<_> {
+            base_image: super::typ::ImageType::Scratch,
+            scripts: vec![1],
+            name: image_name.to_string(),
+            tag: image_tag.to_string(),
+        };
+        let expected_string = format!(
+            r#"---
+scripts:
+  - 1
+base_image:
+  name: {}
+name: {}
+tag: {}
+"#,
+            super::typ::SCRATCH_IMAGE_NAME,
+            image_name,
+            image_tag,
+        );
+        let serialized_string = serde_yaml::to_string(&original_image);
+        assert!(serialized_string.is_ok());
+        assert_eq!(serialized_string.unwrap(), expected_string);
+    }
 }
